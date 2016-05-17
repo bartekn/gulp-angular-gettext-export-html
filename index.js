de58da62ccc191b98978a4f76a2bfa81cb96e57f
Lars Rasmussen <lars@1337.dk>
2016-05-17 16:53:29 +0200

    Refactoring get translation and implementing filter + tag support

--- index.js
+++ index.js
@@ -25,6 +25,21 @@ module.exports = function(poFiles) {
 
   var translations = _.zipObject(languages, _.pluck(parsedPoFiles, 'items'));
 
+  var getTranslation = function(language, string){
+    // Search for available translations
+    for (var i in translations[language]) {
+      var translation = translations[language][i];
+      if (translation.msgid === string) {
+        var translationid = translation.msgid;
+         if(translation.msgstr[0]){
+             return translation.msgstr[0];
+         }
+      }
+    }
+
+    return string;
+  };
+
   return through.obj(function(file, enc, throughCallback) {
     if (file.isNull()) {
       this.push(file);
@@ -47,31 +62,53 @@ module.exports = function(poFiles) {
         var plural;
         var node = $(n);
 
-        var attr = node.attr();
-        if (attr.hasOwnProperty('translate') && !attr.hasOwnProperty('translate-plural')) {
-          plural = node.attr('translate-plural');
-          node.removeAttr('translate');
+        if(n.name.toLowerCase() === "translate"){
           // Search for available translations
-          for (var i in translations[language]) {
-            var translation = translations[language][i];
-            if (translation.msgid === node.text()) {
-              node.text(translation.msgstr[0]);
-              break;
-            }
-          }
+          node.replaceWith(getTranslation(language, node.text()));
+          return;
+        }
+
+        var attr = node.attr();
+
+        // Loop through all translate attributes
+        var translationExist = false;
+        if (attr.hasOwnProperty('translate') && !attr.hasOwnProperty('translate-plural')) {
+          node.removeAttr('translate');
+
+          node.text(getTranslation(language, node.text()));
+        }else if(attr.hasOwnProperty('translate') && attr.hasOwnProperty('translate-plural')){
+          var singular = node.text();
+          var plural = node.attr('translate-plural');
+          var count = node.attr('translate-n');
+
+          node.removeAttr('translate');
+          node.removeAttr('translate-n');
+          node.removeAttr('translate-plural');
+
+          var singularTranslated = getTranslation(language, singular);
+          var pluralTranslated = getTranslation(language, plural).replace('{{$count}}', '{}');
+
+          var ngPluralize = '<ng-pluralize count="' + count + '" when="{\'one\': \'' + singularTranslated + '\', \'other\': \'' + pluralTranslated + '\'}"></ng-pluralize>';
+          node.text(ngPluralize);
         }
 
         if(attr.hasOwnProperty('placeholder')){
           var placeholder = node.attr('placeholder');
           // Search for available translations
-          for (var j in translations[language]) {
-            var t = translations[language][j];
-            if (t.msgid === placeholder) {
-              node.attr('placeholder', t.msgstr[0]);
-              break;
-            }
-          }
+          node.attr('placeholder', getTranslation(language, placeholder));
         }
+
+        // Loop through all attributes which matches regex: {{'string' | translate}}
+        // @example <input type="text" placeholder="{{'string' | translate}}"/>
+        // @see https://regex101.com/r/oS8lJ5/1
+        var regex = /^{{\s?[\'\"](.*)[\'\"]\s?\|\s?translate}}$/;
+        _.forEach(node[0].attribs, function (attribute, key) {
+            var result = attribute.match(regex);
+            if (result) {
+              var translated = getTranslation(language, result[1]);
+              node.attr(key, translated);
+            }
+        }); 
       });
 
       newFile.contents = new Buffer($.html());
